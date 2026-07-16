40f08e561b5637e66ea8d1a1068e935582a569c3
venkatasumanthrayasam <venkatasumanthrayasam@gmail.com>
2026-07-16 22:00:56 +0530

    Initial Commit - Completed operators concept

---
+++ JavaScript_operators/arithematic_operators.js
@@ -0,0 +1,14 @@
+// Arthematic Operators in JavaScript
+
+let a = 10;
+let b = 20;
+let sum = a+b;
+let difference = a-b;
+let multiply = a*b;
+let divide = a/b;
+console.log('Sum Operation:', sum);
+console.log('Substraction Operation:', difference);
+console.log('Multiplication operation:', multiply);
+console.log('Division operation:',divide);
+
+
---
+++ JavaScript_operators/logical_operators.js
@@ -0,0 +1,10 @@
+// Logical Operators in JavaScript && || ! returns boolean value
+
+let n = 10;
+let m = 30;
+
+console.log(n && m);
+console.log(n || m);
+
+
+
